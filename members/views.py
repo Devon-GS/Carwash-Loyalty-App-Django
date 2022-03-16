51f3a00b3f8ceaa9d2438134d3baf2c4a3433b02
Devon <ds.pydev@gmail.com>
2022-03-16 11:23:57 +0200

    add user registration

--- members/views.py
+++ members/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout 
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterUserForm
 
 def login_user(request):
     if request.method == "POST":
@@ -20,4 +22,22 @@ def logout_user(request):
     logout(request)
     messages.success(request, ('You Were Logged Out!'))
     return redirect('home')
+
+def register_user(request):
+    if request.method == "POST":
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            messages.success(request, ('Registration Successful!'))
+            return redirect('home')
+    else:
+        form = RegisterUserForm()    
+
+    return render(request, 'authenticate/register_user.html', {
+        'form': form
+    })
 
